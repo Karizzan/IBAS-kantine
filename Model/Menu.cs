980c20533940132dcc6406b7edd4c829a49e82a0
Marcus Houmark Larsen <marcushoumark@gmail.com>
2024-11-04 11:47:19 +0100

    Yes

---
+++ Model/Menu.cs
@@ -0,0 +1,11 @@
+﻿namespace Model
+{
+	public class Menu
+	{
+		public string Uge { get; set; }
+		public string Ugedag { get; set; }
+		public string Dato { get; set; }
+		public string Varm_Ret { get; set; }
+		public string Kold_Ret { get; set; }
+	}
+}
